0dd2fa345330521da1565e15bd9f991ae634d65c
Leonardo Zaramello <leeurw@hotmail.com>
2021-05-24 20:04:33 -0300

    Exercicios do bloco 4.4, faltando fazer alguns

---
+++ bloco_4/dia_4/ExercicioParte02.js
@@ -0,0 +1,13 @@
+let palavra = 'reviver';
+
+
+function verificaPalindrome(word){
+  let letraComeco = 0;
+  let letraFinal = word.length;
+
+  for(let letraComeco = 0; letraComeco =< letraFinal; letraComeco +=1){
+
+    
+
+  }
+}
