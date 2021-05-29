d95755c9389b41e3fd599f4b208690a999587579
Leonardo Zaramello <leeurw@hotmail.com>
2021-05-29 18:16:28 -0300

    Adicionando alguns exercicios do Bloco 5 dia 3

--- bloco_5/dia_3/main.js
+++ bloco_5/dia_3/main.js
@@ -10,7 +10,7 @@ const myWebpage = document.getElementById('mySpotrybefy');
 1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
  Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 com a classe 'tech';
- Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
+ 2- Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 redirecione para alguma página;
 1. Que tal redirecionar para seu portifólio?
  Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
@@ -18,6 +18,31 @@ a cor do mesmo;
 
 Segue abaixo um exemplo do uso de event.target:
 */
+
+
+let spotFry = document.getElementById ('mySpotrybefy');
+
+
+window.onload = function () {
+
+  input.addEventListener ('click' , function() {
+    
+    console.log(target.innerText);
+    
+})
+
+
+  spotFry.onmouseover = function(){
+  spotFry.style.backgroundColor = 'green';
+  }
+
+  spotFry.onmouseleave = function(){
+  spotFry.style.backgroundColor = '';
+  }
+
+
+}
+
 
 function resetText(event) {
   // O Event é passado como um parâmetro para a função.
